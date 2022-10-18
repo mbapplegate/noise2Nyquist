4490ad4f5b3fd0217800c55df4599c45da5039bc
Matthew Applegate <mapplegate@ece.neu.edu>
2022-10-18 19:42:45 -0400

    minor syntax

--- code/trainingAndTestingScripts/trainAllPhantom.sh
+++ code/trainingAndTestingScripts/trainAllPhantom.sh
@@ -14,43 +14,42 @@
 echo "Running training script..."
 
 #Clean Targets
-python trainOOP_CL.py --dataType phantom --saveDir ../results/phantom --nextImFlag \
---cleanTargets --loss_fn l2 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 1 --randomOrder \
---trainPhantomFile ./HRPhantomData/SheppLoganPhan.mat --valPhantomFile ./HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0
+python ../trainOOP_CL.py --dataType phantom --saveDir ../../results/phantom --nextImFlag \
+--cleanTargets --loss_fn l1 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 1 --randomOrder \
+--trainPhantomFile ../HRPhantomData/SheppLoganPhan.mat --valPhantomFile ../HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0 --nfolds 10 
 
 #Noisy Targets
-python trainOOP_CL.py --dataType phantom --saveDir ../results/phantom \
---loss_fn l2 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 1 --randomOrder \
---trainPhantomFile ./HRPhantomData/SheppLoganPhan.mat --valPhantomFile ./HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0
+python ../trainOOP_CL.py --dataType phantom --saveDir ../../results/phantom \
+--loss_fn l1 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 1 --randomOrder \
+--trainPhantomFile ../HRPhantomData/SheppLoganPhan.mat --valPhantomFile ../HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0 --nfolds 10
 
 #Next Targets (Nyquist)
-python trainOOP_CL.py --dataType phantom --saveDir ../results/phantom --nextImFlag \
+python ../trainOOP_CL.py --dataType phantom --saveDir ../../results/phantom --nextImFlag \
 --loss_fn l1 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 0.5 --randomOrder \
---trainPhantomFile ./HRPhantomData/SheppLoganPhan.mat --valPhantomFile ./HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0
+--trainPhantomFile ../HRPhantomData/SheppLoganPhan.mat --valPhantomFile ../HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0 --nfolds 10
 
 #Next Targets (Nyquist/2)
-python trainOOP_CL.py --dataType phantom --saveDir ../results/phantom --nextImFlag \
---loss_fn l2 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 2 --randomOrder \
---trainPhantomFile ./HRPhantomData/SheppLoganPhan.mat --valPhantomFile ./HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0
+python ../trainOOP_CL.py --dataType phantom --saveDir ../../results/phantom --nextImFlag \
+--loss_fn l1 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 2 --randomOrder \
+--trainPhantomFile ../HRPhantomData/SheppLoganPhan.mat --valPhantomFile ../HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0 --nfolds 10
 
 #Next Targets (Nyquist/3)
-python trainOOP_CL.py --dataType phantom --saveDir ../results/phantom --nextImFlag \
---loss_fn l2 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 3 --randomOrder \
---trainPhantomFile ./HRPhantomData/SheppLoganPhan.mat --valPhantomFile ./HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0
+python ../trainOOP_CL.py --dataType phantom --saveDir ../../results/phantom --nextImFlag \
+--loss_fn l1 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 3 --randomOrder \
+--trainPhantomFile ./HRPhantomData/SheppLoganPhan.mat --valPhantomFile ./HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0 --nfolds 10
 
 #Next Targets (Nyquist/4)
-python trainOOP_CL.py --dataType phantom --saveDir ../results/phantom --nextImFlag \
---loss_fn l2 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 4 --randomOrder\
---trainPhantomFile ./HRPhantomData/SheppLoganPhan.mat --valPhantomFile ./HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0
+python ../trainOOP_CL.py --dataType phantom --saveDir ../../results/phantom --nextImFlag \
+--loss_fn l1 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 4 --randomOrder\
+--trainPhantomFile ../HRPhantomData/SheppLoganPhan.mat --valPhantomFile ../HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0 --nfolds 10
 
 #line2line
-python trainOOP_CL.py --dataType phantom --saveDir ../results/phantom --nextImFlag \
---loss_fn l2 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 1 --singleImage --randomOrder\
---trainPhantomFile ./HRPhantomData/SheppLoganPhan.mat --valPhantomFile ./HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0
+#python trainOOP_CL.py --dataType phantom --saveDir ../results/phantom --nextImFlag \
+#--loss_fn l1 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 1 --singleImage --randomOrder\
+#--trainPhantomFile ./HRPhantomData/SheppLoganPhan.mat --valPhantomFile ./HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0
 
 #noise2void
-python trainOOP_CL.py --dataType phantom --saveDir ../results/phantom --nextImFlag \
---loss_fn l2 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 1 --singleImage --noise2VoidFlag --randomOrder\
---trainPhantomFile ./HRPhantomData/SheppLoganPhan.mat --valPhantomFile ./HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0
+python ../trainOOP_CL.py --dataType phantom --saveDir ../../results/phantom --nextImFlag \
+--loss_fn l1 --batch_size 4 --calcStats --phantomNoiseLevel 45 --sampMult 1 --singleImage --noise2VoidFlag --randomOrder --trainPhantomFile ../HRPhantomData/SheppLoganPhan.mat --valPhantomFile ../HRPhantomData/YuYeWangPhan.mat --numEpochs 150 --splitsToTrain 10 --startSplit 0 --nfolds 10
 
 echo "Done. Hopefully there were no errors"
